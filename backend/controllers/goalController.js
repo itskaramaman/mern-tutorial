b7928fd07bfd610a4ac585db3bb69238a7a83544
Karamjeet Singh Jandu <singhkaramjeetamanapple@gmail.com>
2022-06-25 12:03:23 -0700

    api for goals completed

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,36 +1,57 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalModel");
 
 // @desc    Get goals
 // @route   Get /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Get goals"});
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 })
 
-// @desc    Set goals
+// @desc    Set goalspo
 // @route   Post /api/goals
 // @access  Private
 const setGoals = asyncHandler(async (req, res) => {
-    console.log(req.body);
     if(!req.body.text){
         res.status(400);
         throw new Error("Please add a text field");
     }
-    res.status(200).json({message: "Set goal"});
+
+    const goal = await Goal.create({text: req.body.text});
+    res.status(200).json(goal);
 })
 
 // @desc    Update goals
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update Goal ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal){
+        res.status(400);
+        throw new Error("Goal not found");
+    }
+
+    console.log(req.body.text);
+    const goalUpdated = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true}); // first param tp get goal, second to update and third to create new if it does not exists.
+    console.log(goalUpdated);
+    res.status(200).json(goalUpdated);
 })
 
 // @desc    Delete goals
 // @route   DELET /api/goals/:id
 // @access  Private
 const deleteGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete Goal ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal){
+        res.status(400);
+        throw new Error("Please select the goal to be deleted");
+    }
+
+    await goal.remove();
+    res.status(200).json({id: req.params.id});
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const {errorHandler} = require("./middleware/errorMiddleware");
+const colors = require("colors");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true})); // Body parser to get data from frontend
